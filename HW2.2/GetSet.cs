fb74b8bc6c74d8707e090a3cea4d2f863e6cd302
Zafichi <47737066+Zafichi@users.noreply.github.com>
2019-09-25 00:14:56 +0300

    Create GetSet.cs

---
+++ HW2.2/GetSet.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2._2
+{
+    partial class Motorcicle
+    {
+        public int Milage
+        {
+            get { return milage; }
+            set { milage = value; }
+        }
+        public string Mark
+        {
+            get { return mark; }
+            set { mark = value; }
+        }
+        public bool Trike
+        {
+            get { return trike; }
+            set { trike = value; }
+        }
+        public int EnginePower
+        {
+            get { return enginePower; }
+            set { enginePower = value; }
+        }
+        public int MaxSpeed
+        {
+            get { return maxSpeed; }
+            set { maxSpeed = value; }
+        }
+    }
+}
